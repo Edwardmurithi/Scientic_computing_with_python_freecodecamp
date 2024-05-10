ab456c392c37ce786b6727eae7d86a01113feb65
Edwardmurithi <murithiedward330@gmail.com>
2024-05-10 09:42:17 +0300

    Completed Day 1: Started Python learning journey. Learned basics of input/output, variables, data types, string manipulation, and explored additional topics including string formatting, error handling, and Python documentation. Updated README with summary of Day 1 learning and extra bonus study. Ready to continue learning tomorrow!

---
+++ Day_1/day_1_v1.0.py
@@ -0,0 +1,11 @@
+# Program that prompts the user to enter their name and then prints
+# a greeting message including their name.
+
+# Prompt the user to enter their name
+user_name = input("What is your name? ")
+
+# Create the greeting message by concatenating the user's name
+greetings = "Hello, " + user_name
+
+# Print the greeting message
+print(greetings)
---
+++ Day_1/day_1_v1.2.py
@@ -0,0 +1,37 @@
+def get_valid_name():
+      """User input validation."""
+      while True:
+            user_name = input("What is your name? ")
+            if user_name.strip().isalpha(): # check if input only contain letters.
+                  return user_name.title()
+            else:
+                  print("Invalid name, Please enter only alphabetic characters")
+
+def get_greeting_message(user_name, greeting_choice):
+      greetings = {
+            1: "Hello, {}!".format(user_name),
+            2: "Hi there, {}!".format(user_name),
+            3: "Welcome back, {}!".format(user_name)
+      }
+
+      return greetings.get(greeting_choice, "Hello, {}!".format(user_name))
+
+
+def main():
+      # Prompt the user to enter their name
+      user_name = get_valid_name()
+
+      # Provide option for greeting message
+      print("Choose a greeting message.")
+      print("1. Hello")
+      print("2. Hi there")
+      print("3. Welcome back")
+      greeting_choice = int(input("Enter your choice (1-3): "))
+
+      greeting_message = get_greeting_message(user_name, greeting_choice)
+
+      print(greeting_message)
+
+
+if __name__ == "__main__":
+      main()
