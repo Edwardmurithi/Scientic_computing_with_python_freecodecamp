ed78967457281122cc0383cb4f602b518ec483f7
Edwardmurithi <murithiedward330@gmail.com>
2024-05-10 10:13:02 +0300

    updated v1.2

--- Day_1/day_1_v1.2.py
+++ Day_1/day_1_v1.2.py
@@ -18,20 +18,23 @@ def get_greeting_message(user_name, greeting_choice):
 
 
 def main():
-      # Prompt the user to enter their name
-      user_name = get_valid_name()
+      try:
+            # Prompt the user to enter their name
+            user_name = get_valid_name()
 
-      # Provide option for greeting message
-      print("Choose a greeting message.")
-      print("1. Hello")
-      print("2. Hi there")
-      print("3. Welcome back")
-      greeting_choice = int(input("Enter your choice (1-3): "))
-
-      # Generate and print the greeting message
-      greeting_message = get_greeting_message(user_name, greeting_choice)
-
-      print(greeting_message)
+            # Provide option for greeting message
+            print("Choose a greeting message.")
+            print("1. Hello")
+            print("2. Hi there")
+            print("3. Welcome back")
+            greeting_choice = int(input("Enter your choice (1-3): "))
+            # Generate and print the greeting message
+            greeting_message = get_greeting_message(user_name, greeting_choice)
+            
+            print(greeting_message)
+      except ValueError:
+            print("Invalid input. Please enter a valid choice.")
+            greeting_message = get_greeting_message(user_name, greeting_choice)
 
 
 if __name__ == "__main__":
